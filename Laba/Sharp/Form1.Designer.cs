0c46a1179ed916edf2458196d7bf089c334d5aba
118260 <48309364+118260@users.noreply.github.com>
2020-01-09 17:38:12 +0300

    Add files via upload

---
+++ Laba/Sharp/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Sharp
+{
+    partial class Laba
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Найти просрок";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Location = new System.Drawing.Point(12, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(93, 40);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Visible = false;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(714, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 53);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Диаграмма сколько купили от общего числа";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(186, 232);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 53);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Диаграмма сколько просрочилось от общего числа";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.BorderSkin.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.Center;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 291);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Соотношение просрока к общему количеству количеству";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(531, 300);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "dsfdsfd";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(537, 291);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Соотношение покупок к общему количеству товара";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(465, 300);
+            this.chart2.TabIndex = 6;
+            this.chart2.Text = "chart2";
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(181, 1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 53);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Показать товары, которые нужно заказать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Laba
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 587);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Laba";
+            this.Text = "ProductManager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Laba/Sharp/Form1.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace Sharp
+{
+    public partial class Laba : Form
+    {
+        int TriggerFlag;
+        public Laba()
+        {
+            InitializeComponent();
+            Start();
+
+        }
+
+
+        private void Start()
+        {
+            //Заводим активную Excel книгу и страницу
+            Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook ExcelWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet ExcelWorkSheet;
+
+            //Открытие файла
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Выберите файл";
+            ofd.Filter = "Файл Excel|*.XLSX;*.XLS | Все файлы(*.*)|*.*";
+            ofd.ShowDialog();
+            System.Data.DataTable tb = new System.Data.DataTable();
+            string filename = ofd.FileName;
+            ExcelWorkBook = ExcelApp.Workbooks.Open(ofd.FileName);
+
+            //Создание первой активной страницы
+            ExcelWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ExcelWorkBook.Worksheets.get_Item(1);
+
+
+
+            //Цикл создания колонок DatagridView
+            for (int a = 1; a < 9; a++)
+            {
+
+                dataGridView1.Columns.Add(null, null);
+                dataGridView1.Columns[a - 1].HeaderText = ExcelWorkSheet.Cells[1, a].value;
+            }
+
+
+
+            //Цикл добавление строк и копирования из Excel, с конвертацией типов в String, чтобы не вылезали эксепшены в нескольких местах
+            //да и в целом выглядело опрятнее
+
+            for (int i = 2; ExcelWorkSheet.Cells[i, 4].value != null; i++)
+            {
+                dataGridView1.Rows.Add(null, null, null, null);
+                for (int j = 1; j < 9; j++)
+                {
+
+                    dataGridView1.Rows[i - 2].Cells[j - 1].Value = ExcelWorkSheet.Cells[i, j].value;
+                    String temp = Convert.ToString(dataGridView1.Rows[i - 2].Cells[j - 1].Value);
+                    dataGridView1.Rows[i - 2].Cells[j - 1].Value = temp;
+
+
+                }
+
+
+            }
+            ExcelApp.Quit();
+        }
+
+        private int SummaTovara() {
+            int SumTovara = 0;
+            //подсчет общей суммы количества товара
+            for (int index = 3; dataGridView1.Rows[index - 2].Cells[4].Value != null; index++)
+            {
+                SumTovara = SumTovara + Convert.ToInt32(dataGridView1.Rows[index - 2].Cells[4].Value);
+            }
+            return SumTovara;
+            }
+
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            //Цикл вывода просрока через Message Box
+
+            for (int index = 3; dataGridView1.Rows[index - 2].Cells[3].Value != null; index++)
+            {
+
+                /*Вот тут я мог бы оперировать типами DateTime настоящего времени и даты просрока, но для вычисления разности не работал TimeSpan, 
+                поэтому вот так вот*/
+
+                //=====================================================
+                // Берем тип DateTime даты нашего просрока, конвертируем в String,
+                //убираем лишнее в формате String и конвертируем в int для дальнейших вычислений
+                String FullDate = Convert.ToString(dataGridView1.Rows[index - 2].Cells[3].Value); 
+                    FullDate = FullDate.Remove(10, 8);
+
+                    String SDay = FullDate.Remove(2, 8);
+                    int Day = Convert.ToInt32(SDay);
+
+                    String MonthYear = FullDate.Substring(3);
+                    String SMonth = MonthYear.Remove(2, 5);
+                    int Month = Convert.ToInt32(SMonth);
+
+
+                    String SYear = FullDate.Substring(6);
+                    int Year = Convert.ToInt32(SYear);
+
+
+                //=====================================================
+                //!!!!! Берем тип DateTime настоящего времени, конвертируем в String, убираем лишнее в формате String
+                //И конвертируем в int для дальнейших вычислений
+
+                DateTime DTNow = DateTime.Now;
+                    String Now = DTNow.ToShortDateString();
+
+
+                    String SNDay = Now.Remove(2, 8);
+                    int NDay = Convert.ToInt32(SNDay);
+
+                    String SNMonthYear = Now.Substring(3);
+                    String SNMonth = SNMonthYear.Remove(2, 5);
+                    int NMonth = Convert.ToInt32(SNMonth);
+
+
+                    String SNYear = Now.Substring(6);
+                    int NYear = Convert.ToInt32(SNYear);
+
+                //Здесь мы просто проверяем на просрок через условие и ставим тригеру 0/1 в зависимости просрочилось ли
+                // 1 - просрок, 0 - будет ещё лежать
+                    if ((NYear == Year) && (NMonth == Month) && (NDay == Day))
+                    {
+                        TriggerFlag = 1; 
+                    }
+
+                    else if ((NYear == Year) && (NMonth == Month) && (NDay != Day))
+                    {
+                        if (NDay > Day)
+                        {
+                            TriggerFlag = 1;
+                        }
+                        else
+                        {
+                            TriggerFlag = 0;
+                        }
+                    }
+                    else if ((NYear == Year) && (NMonth != Month))
+                    {
+                        if (NMonth > Month)
+                        {
+                            TriggerFlag = 1;
+                        }
+                        else
+                        {
+                            TriggerFlag = 0;
+                        }
+                    }
+                    else if (NYear != Year)
+                    {
+                        if (NYear > Year)
+                        {
+                            TriggerFlag = 1;
+                        }
+                        else
+                        {
+                            TriggerFlag = 0;
+                        }
+                    }
+
+                    // Тут мы проверяем, если флаг равен 1, то он выводит пушик через Massagebox о том, какой товар просрочился
+
+                    if (TriggerFlag == 1)
+                    {
+                        String ID = dataGridView1.Rows[index - 2].Cells[0].Value.ToString();
+                        String VendorCode = dataGridView1.Rows[index - 2].Cells[1].Value.ToString();
+                        String NameProduct = dataGridView1.Rows[index - 2].Cells[2].Value.ToString();
+                        String FullName = "ID:" + ID + " " + "Артикул:" + VendorCode + " " + "Наименование:" + NameProduct;
+                        MessageBox.Show(FullName, "ПРОСРОЧЕНО");
+
+                    }
+                 
+    }
+            //
+
+            
+
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //нахождение общей суммы товара
+            int SumTovara = SummaTovara();
+            int SumPokupok = 0;
+            //цикл нахождения суммы покупок
+            for (int index = 3; dataGridView1.Rows[index - 2].Cells[5].Value != null; index++)
+            {
+                SumPokupok = SumPokupok + Convert.ToInt32(dataGridView1.Rows[index - 2].Cells[5].Value);
+            }
+
+
+
+
+                chart2.Series["Соотношение покупок к общему количеству товара"].Points.AddXY("Общее количество", SumTovara);
+            chart2.Series["Соотношение покупок к общему количеству товара"].Points.AddXY("Покупки", SumPokupok);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int SumProsrok = 0;
+            //цикл нахождение просрока + сумма просроченного товара
+            for (int index = 3; dataGridView1.Rows[index - 2].Cells[3].Value != null; index++)
+            {
+
+                /*Вот тут я мог бы оперировать типами DateTime настоящего времени и даты просрока, но для вычисления разности не работал TimeSpan, 
+                поэтому вот так вот*/
+
+                //=====================================================
+                // Берем тип DateTime даты нашего просрока, конвертируем в String,
+                //убираем лишнее в формате String и конвертируем в int для дальнейших вычислений
+                String FullDate = Convert.ToString(dataGridView1.Rows[index - 2].Cells[3].Value);
+                FullDate = FullDate.Remove(10, 8);
+
+                String SDay = FullDate.Remove(2, 8);
+                int Day = Convert.ToInt32(SDay);
+
+                String MonthYear = FullDate.Substring(3);
+                String SMonth = MonthYear.Remove(2, 5);
+                int Month = Convert.ToInt32(SMonth);
+
+
+                String SYear = FullDate.Substring(6);
+                int Year = Convert.ToInt32(SYear);
+
+
+                //=====================================================
+                //!!!!! Берем тип DateTime настоящего времени, конвертируем в String, убираем лишнее в формате String
+                //И конвертируем в int для дальнейших вычислений
+
+                DateTime DTNow = DateTime.Now;
+                String Now = DTNow.ToShortDateString();
+
+
+                String SNDay = Now.Remove(2, 8);
+                int NDay = Convert.ToInt32(SNDay);
+
+                String SNMonthYear = Now.Substring(3);
+                String SNMonth = SNMonthYear.Remove(2, 5);
+                int NMonth = Convert.ToInt32(SNMonth);
+
+
+                String SNYear = Now.Substring(6);
+                int NYear = Convert.ToInt32(SNYear);
+
+                //Здесь мы просто проверяем на просрок через условие и ставим тригеру 0/1 в зависимости просрочилось ли
+                // 1 - просрок, 0 - будет ещё лежать
+                if ((NYear == Year) && (NMonth == Month) && (NDay == Day))
+                {
+                    TriggerFlag = 1;
+                }
+
+                else if ((NYear == Year) && (NMonth == Month) && (NDay != Day))
+                {
+                    if (NDay > Day)
+                    {
+                        TriggerFlag = 1;
+                    }
+                    else
+                    {
+                        TriggerFlag = 0;
+                    }
+                }
+                else if ((NYear == Year) && (NMonth != Month))
+                {
+                    if (NMonth > Month)
+                    {
+                        TriggerFlag = 1;
+                    }
+                    else
+                    {
+                        TriggerFlag = 0;
+                    }
+                }
+                else if (NYear != Year)
+                {
+                    if (NYear > Year)
+                    {
+                        TriggerFlag = 1;
+                    }
+                    else
+                    {
+                        TriggerFlag = 0;
+                    }
+                }
+
+                
+                
+                if (TriggerFlag == 1)
+                {
+                    SumProsrok = Convert.ToInt32(dataGridView1.Rows[index - 2].Cells[6].Value) + SumProsrok;
+
+                }
+            }
+           //вызов функции подсчета количества товара
+            int SumTovara = SummaTovara();
+            //отображение на диаграмме
+            chart1.Series["Соотношение просрока к общему количеству количеству"].Points.AddXY("Общее количество", SumTovara);
+            chart1.Series["Соотношение просрока к общему количеству количеству"].Points.AddXY("Просрок", SumProsrok);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int  SaleInDay;
+            int Ost;
+            int temp;
+            //цикл вывода сообщения что нужно заказать
+            for (int index = 3; dataGridView1.Rows[index - 2].Cells[6].Value != null; index++)
+            {
+                
+                SaleInDay = Convert.ToInt32(dataGridView1.Rows[index - 2].Cells[7].Value);
+                Ost = Convert.ToInt32(dataGridView1.Rows[index - 2].Cells[6].Value);
+                temp = Ost / SaleInDay;
+                if (temp < 3) {
+                    String ID = dataGridView1.Rows[index - 2].Cells[0].Value.ToString();
+                    String VendorCode = dataGridView1.Rows[index - 2].Cells[1].Value.ToString();
+                    String NameProduct = dataGridView1.Rows[index - 2].Cells[2].Value.ToString();
+                    String FullName = "ID:" + ID + " " + "Артикул:" + VendorCode + " " + "Наименование:" + NameProduct;
+                    MessageBox.Show(FullName, "Нужно заказать");
+                }
+            }
+            }
+    }
+    
+
+}
+
